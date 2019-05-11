9342146624cb97579bce211c6a2fa463f25ee51b
catmao <cat841230@gmail.com>
2019-05-11 22:39:59 +0800

    Modify supoort python version.

--- setup.py
+++ setup.py
@@ -10,12 +10,6 @@ setup(
     packages=['simplehttp'],
     classifiers=[
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.0',
-        'Programming Language :: Python :: 3.1',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
