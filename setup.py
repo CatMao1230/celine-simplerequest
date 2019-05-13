e6893b4942f20531135a0c33eb02c70b894718ae
catmao <cat841230@gmail.com>
2019-05-13 19:45:05 +0800

    Find packages exclude tests.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='celine-simplerequest',
-    version='1.0.3',
+    version='1.0.4',
     author='Celine Yeh',
     author_email='cat841230@gmail.com',
     description='A simple request.',
     url='https://github.com/CatMao1230/celine-simplerequest',
-    packages=['simplehttp'],
+    packages=find_packages(exclude=['tests']),
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
